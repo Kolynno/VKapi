e743f5538a0240cf93d41e365b62d03a6027a89b
Nickolay Zhmakin <nickzhmakin@mail.ru>
2023-11-24 17:27:00 +0300

    main-view style done

--- src/main/java/nick/itmo/vkapi/controllers/MainViewController.java
+++ src/main/java/nick/itmo/vkapi/controllers/MainViewController.java
@@ -1,4 +1,20 @@
 package nick.itmo.vkapi.controllers;
 
+import javafx.event.ActionEvent;
+
 public class MainViewController {
+    public void ButtonSendPostClick(ActionEvent actionEvent) {
+    }
+
+    public void ButtonClearTextClick(ActionEvent actionEvent) {
+    }
+
+    public void ButtonClearVarsClick(ActionEvent actionEvent) {
+    }
+
+    public void ButtonDeleteTemplateClick(ActionEvent actionEvent) {
+    }
+
+    public void ButtonSaveTemplateClick(ActionEvent actionEvent) {
+    }
 }
